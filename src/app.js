a0e4a5f6e8df499a68bb81828bd51b657bdc52e1
Thiago Pereira <thiago.spereira02@gmail.com>
2021-06-17 16:12:55 -0300

    Post of games route

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('/categories', async (req, res) => {
 
 app.post('/categories', async (req, res) => {
     const { name } = req.body;
-    const userSchema = joi.object({
+    const categoriesSchema = joi.object({
         name: joi.string().trim().required()
     });
     
@@ -40,7 +40,7 @@ app.post('/categories', async (req, res) => {
         if (isRepeated.rows[0]){
             return res.sendStatus(409);
         }
-        const isValid = userSchema.validate({name});
+        const isValid = categoriesSchema.validate({name});
         if (isValid.error === undefined){
             await connection.query('INSERT INTO categories (name) VALUES ($1)', [name]);
             res.sendStatus(201); 
@@ -52,8 +52,47 @@ app.post('/categories', async (req, res) => {
 
     } catch(e) {
         console.log(e);
-        res.send(500);
+        res.sendStatus(500);
     }   
+});
+
+/* Games routes */
+
+app.get('/games', async (req, res) => {
+    
+})
+
+app.post('/games', async(req, res) => {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    const gamesSchema = joi.object({
+        name: joi.string().trim().required(),
+        image: joi.string().uri().pattern(/^http([^\s]+(?=\.(jpg|gif|png))\.\2)/).required(),
+        stockTotal: joi.number().integer().min(1).required(),
+        categoryId: joi.number().integer().min(1).required(),
+        pricePerDay: joi.number().integer().min(1).required()
+    });
+    
+    try{
+        const isRepeated = await connection.query('SELECT * FROM games WHERE name LIKE $1', [name]);
+        if (isRepeated.rows[0]){
+            return res.sendStatus(409);
+        }
+        const categoryIdExists = await connection.query('SELECT * FROM categories WHERE id = $1', [categoryId]);
+        if (!categoryIdExists.rows[0]){
+            return res.sendStatus(400);
+        }
+        const isValid = gamesSchema.validate({name, image, stockTotal, categoryId, pricePerDay});
+        if (isValid.error === undefined){
+            await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5)', [name, image, stockTotal, categoryId, pricePerDay]);
+            return res.sendStatus(201); 
+        } else{
+            return res.sendStatus(400)
+        }
+
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    } 
 });
 
 
